3e6765f1f088afde53ea986b1c6e91b829879f92
Waldron-Wang <152358943+Waldron-Wang@users.noreply.github.com>
2024-04-06 00:29:38 +0800

    character jump

--- 2D URP animation/Assets/script/Black/black_controller.cs
+++ 2D URP animation/Assets/script/Black/black_controller.cs
@@ -8,7 +8,8 @@ public class black_controller : MonoBehaviour
     Animator animator_black;
     SpriteRenderer spriteRenderer_balck;
 
-    [SerializeField] float move_speed;   //移动的速度velocit
+    [SerializeField] float run_speed;   //移动的速度velocity
+    [SerializeField] float walk_speed;   //移动的速度velocity
     [SerializeField] float jump_force_1; //跳跃的力度force
     [SerializeField] float jump_force_2; //二段跳的力度froce
     float move_horizontal;
@@ -21,6 +22,7 @@ public class black_controller : MonoBehaviour
     //最大跳跃次数
     bool move_vertical;
     //检测玩家是否有跳跃输入
+    bool is_face_right;
 
     RaycastHit2D object_being_hit;
     //这是被射线碰撞的结构体实例，包含有关碰撞的详细信息，如碰撞点的位置、碰撞点的表面法线、碰撞物体的引用
@@ -30,7 +32,7 @@ public class black_controller : MonoBehaviour
     //射线的方向
     [SerializeField] float ray_distance;
     //射线的长度,同时也是人物开始进入jump_state 3时的离地面的距离
-    int hit_layer = 1 << 3;
+    [SerializeField] LayerMask ray_layer;
     //使射线只检测30层里的碰撞ti
     // Start is called before the first frame update
     void Start()
@@ -38,6 +40,8 @@ public class black_controller : MonoBehaviour
         rb_black = gameObject.GetComponent<Rigidbody2D>();
         animator_black = gameObject.GetComponent<Animator>();
         spriteRenderer_balck = gameObject.GetComponent<SpriteRenderer>();
+
+        is_face_right = true;
 
         jump_count = 0;
         jump_count_max = 2;
@@ -51,34 +55,41 @@ public class black_controller : MonoBehaviour
         move_horizontal = Input.GetAxisRaw("Horizontal");
         //unity的输入系统被设置为与帧更新同步
 
+        if (move_horizontal > 0.01f && is_face_right == false)
+        {
+            flip();
+        }
+        else if (move_horizontal < -0.01f && is_face_right == true)
+        {
+            flip();
+        }
+
         ray_origin = new Vector2(transform.position.x, transform.position.y);
         if (jump_count == 0) //不用速度作为人物是否在地面的原因是人物的部分空中攻击动作是悬停在空中的
         {
             jump_state = 0;
-            animator_black.SetInteger("jump_state", 0);
         }
-        else if(rb_black.velocity.y > 0.001f)
+        else if (rb_black.velocity.y > 0.001f)
         {
             jump_state = 1;
-            animator_black.SetInteger("jump_state", 1);
         }
         else if (rb_black.velocity.y < -0.001f)
         {
-            object_being_hit = Physics2D.Raycast(ray_origin, ray_direction, ray_distance, hit_layer);
-            if(object_being_hit.collider != null)
+            object_being_hit = Physics2D.Raycast(ray_origin, ray_direction, ray_distance, ray_layer);
+            if (object_being_hit.collider != null)
             {
-                Debug.Log("Hit " + object_being_hit.collider.gameObject.name);
-                jump_state = 3;
-                animator_black.SetInteger("jump_state", 3);
+                if(jump_state != 3)
+                //使land_trigger仅被触发一次，防止land动画后land_trigger依然处于触发状态
+                {
+                    animator_black.SetTrigger("land_trigger");
+                    jump_state = 3;
+                }
             }
             else
             {
                 jump_state = 2;
-                animator_black.SetInteger("jump_state", 2);
             }
         }
-        Debug.Log("jump_state" + jump_state);
-        Debug.Log("jump_count" + jump_count);
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -91,7 +102,18 @@ public class black_controller : MonoBehaviour
 
     private void FixedUpdate()
     {
-        movement();
+        if (Input.GetKey(KeyCode.Tab) == true)
+        {
+            animator_black.SetBool("is_running", false);
+            //当玩家从run切换到walk时，update不会再调用run(),is_running不能被正常重置为false，导致动画出错，故添加此语句
+            walk();
+        }
+        else
+        {
+            animator_black.SetBool("is_walking", false);
+            //同上
+            run();
+        }
 
         if (move_vertical)
         {
@@ -100,20 +122,12 @@ public class black_controller : MonoBehaviour
         }
     }
 
-    void movement()
+    void run()
     {
-        if(move_horizontal > 0.1f || move_horizontal < -0.1f)
+        if (move_horizontal > 0.1f || move_horizontal < -0.1f)
         {
-            rb_black.velocity = new Vector3(move_horizontal * move_speed, rb_black.velocity.y, 0f);
+            rb_black.velocity = new Vector3(move_horizontal * run_speed, rb_black.velocity.y, 0f);
             animator_black.SetBool("is_running", true);
-            if (move_horizontal > 0.1f)
-            {
-                spriteRenderer_balck.flipX = false;
-            }
-            else
-            {
-                spriteRenderer_balck.flipX = true;
-            }
         }
         else
         {
@@ -122,16 +136,31 @@ public class black_controller : MonoBehaviour
         }
     }
 
+    void walk()
+    {
+        if (move_horizontal > 0.1f || move_horizontal < -0.1f)
+        {
+            rb_black.velocity = new Vector3(move_horizontal * walk_speed, rb_black.velocity.y, 0f);
+            animator_black.SetBool("is_walking", true);
+        }
+        else
+        {
+            rb_black.velocity = new Vector3(0f, rb_black.velocity.y, 0f);
+            animator_black.SetBool("is_walking", false);
+        }
+    }
+
     void jump()
     {
         if (jump_count < jump_count_max)
         {
-            if(jump_count == 0)
+            animator_black.SetTrigger("jump_trigger");
+
+            if (jump_count == 0)
             {
                 if (jump_state == 2 || jump_state == 3)
                 {
                     jump_count++;
-                    animator_black.SetInteger("jump_count", jump_count);
                 }
                 //对于并没有进行一段跳跃，因跑出平台而发生坠落的情况：只允许跳一次
                 rb_black.AddForce(new Vector3(0f, jump_force_1, 0f), ForceMode2D.Impulse);
@@ -141,9 +170,14 @@ public class black_controller : MonoBehaviour
                 rb_black.velocity = new Vector3(rb_black.velocity.x, 0f, 0f);
                 rb_black.AddForce(new Vector3(0f, jump_force_2, 0f), ForceMode2D.Impulse);
             }
-            jump_count ++;
-            animator_black.SetInteger("jump_count", jump_count);
+            jump_count++;
         }
+    }
+
+    void flip()
+    {
+        is_face_right = !is_face_right;
+        transform.Rotate(0, 180, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -151,7 +185,6 @@ public class black_controller : MonoBehaviour
         if (collision.gameObject.tag == "platforms")
         {
             jump_count = 0;
-            animator_black.SetInteger("jump_count", 0);
         }
     }
 }
---
+++ 2D URP animation/Assets/script/Black/sword_shield.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sword_shield : MonoBehaviour
+{
+    int attack_count;
+    int attack_max_count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attack_count = 0;
+        attack_max_count = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void attack()
+    {
+
+    }
+}
