4babebfda08686d672f700307178568208fdd1da
Waldron-Wang <152358943+Waldron-Wang@users.noreply.github.com>
2024-04-19 00:39:56 +0800

    daliy update

--- 2D URP animation/Assets/script/Black/sword_shield.cs
+++ 2D URP animation/Assets/script/Black/sword_shield.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class sword_shield : MonoBehaviour
 {
-    int attack_count;
-    int attack_max_count;
+    Animator ss_animator; //sword_shield的animator组件
 
+    bool attack_input;
+    //标示玩家是否有攻击输入
+    bool attack_next_input;
+    //标示在玩家攻击过程中是否有下一次攻击的输入
+    bool is_cheacking_attack_input;
+    //现在是否在攻击输入检测时间段内
+    bool is_attacking;
+    //是否处于攻击状态
+    
     // Start is called before the first frame update
     void Start()
     {
-        attack_count = 0;
-        attack_max_count = 3;
+        ss_animator = gameObject.GetComponent<Animator>();
+
+        attack_input = false;
+        attack_next_input = false;
+        is_attacking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J) && is_attacking == false)
+        {
+            attack_input = true;
+            is_attacking = true;
+        }
+
+        if (is_cheacking_attack_input)
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+                attack_next_input = true;
+            }
+        }
     }
 
-    void attack1()
+    private void FixedUpdate()
     {
+        if (attack_input || attack_next_input)
+        {
+            ss_animator.SetTrigger("attack_trigger");
+        }
+    }
 
+    void start_check()
+    {
+        is_cheacking_attack_input = true;
+    }
+
+    void end_check()
+    {
+        is_cheacking_attack_input = false;
     }
 }
